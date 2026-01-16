79b92e82f47535253d4eed5c194d16fd35c8412f
fangtexi <2395141263@qq.com>
2026-01-16 14:11:24 +0800

    init

---
+++ src/main/java/com/oncallagentjava/OncallAgentJavaApplication.java
@@ -0,0 +1,13 @@
+package com.oncallagentjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OncallAgentJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OncallAgentJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oncallagentjava/OncallAgentJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oncallagentjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OncallAgentJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
